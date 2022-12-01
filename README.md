d30fdf482ca18619437b6ae9cd31f25db765fed2
Gabriel Pizarro <gpizarro@javaman.net>
2022-12-01 10:32:00 -0800

    Initial commit + day 1

---
+++ README.md
@@ -0,0 +1,5 @@
+# advent-of-code-2022
+
+Solving [adventofcode.com/2022](https://adventofcode.com/2022) to practice Rust :)
+
+In order to run a solution, you'll have to copy your unique `input.txt` file to `src/bin/input` as `day{N}.txt`.
---
+++ src/bin/day1.rs
@@ -0,0 +1,20 @@
+use itertools::Itertools;
+
+fn main() {
+    let input = include_str!("input/day1.txt");
+    let elf_sums = input
+        .split("\n\n")
+        .map(|goodies| {
+            goodies.split("\n")
+                .map(|snack| snack.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .sorted()
+        .rev();
+
+    let top1 = elf_sums.clone().max().unwrap();
+    println!("1. Top 1: {top1}");
+
+    let top3: u32 = elf_sums.take(3).sum();
+    println!("2. Top 3: {top3}");
+}
